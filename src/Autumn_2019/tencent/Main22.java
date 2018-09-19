eb80173fa53c99f67a3ca05ee2b957f5724de09f
任志成 <jasonren@JasonRenMBP.local>
2018-09-19 16:18:00 +0800

    update

--- src/Autumn_2019/tencent/Main22.java
+++ src/Autumn_2019/tencent/Main22.java
@@ -22,17 +22,23 @@ public class Main22 {
 
         int cnt = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0)
+            if (nums[i] > 0) //若该节点的入度大于出度说明是重要城市，入度加出度减
                 cnt++;
         }
         System.out.println(cnt);
     }
 
+    /**
+     *
+     * @param idx 此次dfs出度计算节点tar可达的某个节点，但出度并不计算在该节点上
+     * @param tar 此次dfs的出度计算节点
+     * @param vis dfs已经访问过的且已经计算过入度的节点
+     */
     private static void dfs(boolean[][] nodes, int idx, int tar, int[] nums, boolean[] vis) {
         for (int i = 1; i < nodes.length; i++) {
             if (i != idx && nodes[idx][i] && !vis[i]) {
-                nums[tar]--;
-                nums[i]++;
+                nums[tar]--; //出度减
+                nums[i]++;   //入度加
                 vis[i] = true;
                 dfs(nodes, i, tar, nums, vis);
             }
--- src/Autumn_2019/tencent/Test.java
+++ src/Autumn_2019/tencent/Test.java
@@ -1,6 +1,6 @@
 package Autumn_2019.tencent;
 
-public class Terst {
+public class Test {
     public static void main(String[] args) {
         System.out.println(15 * 91 % 16);
     }
